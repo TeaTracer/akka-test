add7e1e3e20b8103de50ec028f31cdb1d3fd1f41
TeaTracer <teatracer7@yandex.ru>
2016-11-20 01:58:41 +0300

    Fix program. Remove server code. Rewrite client code.

--- src/main/scala/JQtest.scala
+++ src/main/scala/JQtest.scala
@@ -1,107 +1,109 @@
+import scala.util.{Success, Failure}
 import scala.concurrent.Future
-import scala.concurrent.Await
-import scala.concurrent.duration._
+import akka.actor._
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import akka.http._
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.HttpMethods._
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.unmarshalling.Unmarshal
-import akka.http.scaladsl.server.PathMatchers.IntNumber
-import akka.http.scaladsl.server.Directives._
-import akka.http._
-import scala.util.{Success, Failure}
-import akka.actor._
-import akka.stream.scaladsl.Flow
+import akka.http.scaladsl.unmarshalling._
 import akka.http.scaladsl.client._
-import akka.http.scaladsl.model.StatusCodes._
+import akka.stream.scaladsl.{Source, Sink}
 import io.circe._
 import io.circe.parser._
-import scala.collection.mutable.HashMap
-import akka.stream.scaladsl.{ Source, Sink  }
+import io.circe.syntax._
 
-object HelloWorld {
+/* Example of remote REST API to fetch data */
+object RemoteAPI {
+  val clientPort = 443 // https
+  val clientHost = "api.jqestate.ru"
+  /* remote url */
+  val apiUrl = "https://api.jqestate.ru/v1/properties/country"
 
-  implicit val system = ActorSystem("Tea")
+  /* pagination query */
+  def pQuery(offset: Int, limit: Int): String = apiUrl + s"?pagination[offset]=$offset&pagination[limit]=$limit"
+
+  /* just one of pages to fetch common data */
+  def onePage = pQuery(0, 0)
+}
+
+object AkkaTest {
+  // akka magic implicits
+  implicit val system = ActorSystem("MagicActorSystem")
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
 
-  val serverPort = 8080
-  val clientPort = 8080
-  val serverHost = "localhost"
-  val clientHost = "localhost"
-  val nRequests = 42
-  val http = Http()
+  /* https connection to REST API server */
+  lazy val testConnection = Http().outgoingConnectionHttps(RemoteAPI.clientHost, RemoteAPI.clientPort)
 
-  val JQApiUrlPropertiesCountry = "https://api.jqestate.ru/v1/properties/country"
+  def getRequest(request: String): HttpRequest = RequestBuilding.Get(request)
 
-  val getPaginationQuery = { offset: Int, limit: Int => {
-    return JQApiUrlPropertiesCountry + s"?pagination[offset]=$offset&pagination[limit]=$limit"
-  }}
-
-  val getTotalFromResponse = (response) => {
-      val string = Unmarshal(response.entity).to[String] // get body string (will be json soon)
-      val doc: Json = parse(string).getOrElse(Json.Null)
-      val cursor: HCursor = doc.hcursor
-      cursor.downField("pagination").downField("total").as[Int]
+/* send request stream pipeline */
+  def sendRequest(request: HttpRequest): Future[HttpResponse] = {
+    println(s"Send request: ${request.uri}")
+    Source.single(request).via(testConnection).runWith(Sink.head)
   }
 
-  val getTotal = () => {
-    val request = getPaginationQuery(0, 0)
-    val response = sendRequest(request)
-    getTotalFromResponse(response)
+  /* request first page and parse total number of pages */
+ def paginationLength: Future[Int] = {
+    val request: HttpRequest = getRequest(RemoteAPI.onePage)
+
+    val response: Future[String] = sendRequest(request) flatMap { r =>
+      Unmarshal(r.entity).to[String] }
+
+    val json: Future[Json] = response flatMap ( x =>
+        Future{ parse(x).getOrElse(Json.Null) })
+
+    val resultDecoder: Future[Decoder.Result[Int]] = json flatMap ( j =>
+        Future{ j.hcursor.downField("pagination").downField("total").as[Int] })
+
+    val result: Future[Int] = resultDecoder flatMap ( x =>
+        Future { x match {
+          case cats.data.Xor.Right(i) => i
+          case j  => {println("get ", j); throw new Exception("Can't parse!")} }})
+
+    result
   }
 
-  val getRequestsStrings = () => {
-    val total = getTotal()
-    val limit = 256 // max for this server
-    val n = total / limit + 1 // number of requests
-    val requests = {1 to n}.map( x => getPaginationQuery((x - 1) * limit, limit))
+/* get response as a string */
+  def fetchResponse(s: String): Future[String] = {
+    val request: HttpRequest = getRequest(s)
+    val response: Future[String] = sendRequest(request) flatMap { response =>
+      Unmarshal(response.entity).to[String] }
+
+    response
+  }
+
+/* prepare and send requests */
+  def getRequestsStrings(n: Int): Future[List[String]] =  {
+    val limit = 256 // elements per page
+    val total: Future[Int] = paginationLength // total elements
+    val requests: Future[List[String]] = total flatMap ((t:Int) => Future {
+        // val n = t / limit + 1 // number of requests
+        val requests = {0 to n}.map(x => RemoteAPI.pQuery(x*limit, limit))
+        requests.toList
+    })
+
     requests
   }
 
-
-  // server roots
-  val route = get {
-    pathSingleSlash {
-      complete {
-          "Hello world!"
-      }
-    } ~
-    pathPrefix(IntNumber) { i =>
-      complete {
-        Future {
-          jsonList(i).nospaces
-        }
-      }
-    }
-  }
-
-  // stream flow
-  val testConnection = http.outgoingConnection(clientHost, clientPort)
-
-  // Stream pipeline: Source(request) | Flow | Sink
-  val testRequest = { request: HttpRequest => {
-    println(s"Request: ${request.uri}")
-    Source.single(request).via(testConnection).runWith(Sink.head)
-  }}
-
-  // request to route '/<i:int>' and get int in response
-  val testFetch = { request: String => {
-    val requestGet = RequestBuilding.Get(request)
-    testRequest(requestGet).flatMap { response =>
-      Unmarshal(response.entity).to[String] // get body string (will be json soon)
-    }
-  }}
-
+/* start point */
   def main(args: Array[String]): Unit = {
-    println(s"Start server at $serverHost:$serverPort")
-    Http().bindAndHandle(route, serverHost, serverPort) // run server
-
-    {1 to nRequests}.foreach(i => { // send test requests
-      testFetch(i) onSuccess {
-        case s: String => println(s"Response: $s")
+    val testNumberOfRequests = 2
+    def doTest() = getRequestsStrings(testNumberOfRequests).onComplete({
+      case Success(l) => {
+        l.foreach(request => {
+          fetchResponse(request).onComplete({
+            case Success(s) => println("Response: ", s slice (0, 42))
+            case Failure(e) => throw new Exception(e)
+          })
+        })
       }
+      case Failure(e) => throw new Exception(e)
     })
+    doTest()
+    sys.exit(0)
   }
 }
